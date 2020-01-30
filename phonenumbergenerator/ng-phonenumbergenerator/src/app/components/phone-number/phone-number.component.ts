bde2d64776d71a4a8d10af96e328dd2eaeda5f95
Satya <satya@kalibrilabs.com>
2020-01-29 23:14:58 -0500

    Updated UI with pagination and tel validation

--- phonenumbergenerator/ng-phonenumbergenerator/src/app/components/phone-number/phone-number.component.ts
+++ phonenumbergenerator/ng-phonenumbergenerator/src/app/components/phone-number/phone-number.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, OnDestroy } from '@angular/core';
 import { ApiRequestService } from '../../services/api-request.service';
 import { Subscription } from 'rxjs';
 
@@ -7,11 +7,19 @@ import { Subscription } from 'rxjs';
   templateUrl: './phone-number.component.html',
   styleUrls: ['./phone-number.component.css']
 })
-export class PhoneNumberComponent implements OnInit {
+export class PhoneNumberComponent implements OnInit, OnDestroy {
 
-  phoneNumber: number;
+  phoneNumberCombinations: string[] = [];
 
-  phoneNumberCombinations: number[] = []
+  currentPageData: string[] = [];
+
+  pageSize = 25;
+
+  // Zero based index
+  pageStart: number;
+
+  // One based index
+  pageEnd: number;
 
   subscription: Subscription;
 
@@ -20,22 +28,63 @@ export class PhoneNumberComponent implements OnInit {
   ngOnInit() {
   }
 
-  fetchCombinations(){
-    if(this.phoneNumber){
+  fetchCombinations(formVal: any) {
+    this.phoneNumberCombinations = [];
+    this.currentPageData = [];
+    if (formVal.phoneNumber) {
       this.phoneNumberCombinations = [];
-      if(this.subscription){
+      if (this.subscription) {
         this.subscription.unsubscribe();
       }
-      this.subscription = this.apiRequest.getPhoneNumberCombinations(this.phoneNumber).subscribe( combinations => {
+      this.subscription = this.apiRequest.getPhoneNumberCombinations(formVal.phoneNumber).subscribe(combinations => {
         this.phoneNumberCombinations = combinations;
+        this.initPagination();
       });
     }
   }
 
-  ngOnDestroy(){
-    if(this.subscription){
-        this.subscription.unsubscribe();
-      }
+  initPagination() {
+    if (!this.phoneNumberCombinations || this.phoneNumberCombinations.length === 0) {
+      return;
+    }
+    this.pageStart = 0;
+    this.pageEnd = this.isPagingEnabled() ? this.pageSize : this.phoneNumberCombinations.length;
+    this.currentPageData = this.phoneNumberCombinations.slice(this.pageStart, this.pageEnd);
+  }
+
+  isPagingEnabled(): boolean {
+    return this.phoneNumberCombinations.length > this.pageSize;
+  }
+
+  nextPage(): void {
+    if (this.hasNext()) {
+      this.pageStart = this.pageEnd;
+      const newPageEnd: number = this.pageEnd + this.pageSize;
+      this.pageEnd = newPageEnd > this.phoneNumberCombinations.length ? this.phoneNumberCombinations.length : newPageEnd;
+      this.currentPageData = this.phoneNumberCombinations.slice(this.pageStart, this.pageEnd);
+    }
+  }
+
+  previousPage(): void {
+    if (this.hasPrevious()) {
+      this.pageEnd = this.pageStart;
+      this.pageStart -= this.pageSize;
+      this.currentPageData = this.phoneNumberCombinations.slice(this.pageStart, this.pageEnd);
+    }
+  }
+
+  hasNext(): boolean {
+    return this.pageEnd < this.phoneNumberCombinations.length;
+  }
+
+  hasPrevious(): boolean {
+    return this.pageStart !== 0;
+  }
+
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
   }
 
 }
